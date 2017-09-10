bddf7ba8d3261fd38b194bbdf662b85e932c25c4
realjbtytm <businessjbtytm@gmail.com>
2017-09-09 20:02:26 -0400

    Base files

---
+++ src/ga/raindrop/Daylight/Game.java
@@ -0,0 +1,18 @@
+package ga.raindrop.Daylight;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created By RainDropInMCYT // RealJBTYTM on 9/9/2017.
+ * All Rights Reserved.
+ */
+
+public class Game {
+
+    public JFrame gamewindow;
+    public Container con;
+    public JPanel titleNamePanel, startButtonPanel, authorNamePanel, mainTextPanel, choiceButtonPanel, playerPanel;
+    public JLabel titleNameLabel, authorNameLabel, hpLabel, hpLabelNumber, weaponLabel, weaponLabelName;
+    public Font titleFont, 
+}
